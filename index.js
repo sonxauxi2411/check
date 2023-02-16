194bc9b334a7180585795476862ee35347f92b34
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-02-16 19:29:03 +0700

    update index

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-const cors = require("cors");
+const cros = require("cors");
+const mongoose = require("mongoose");
+const donte = require("dotenv").config();
+const router = require("./routers/index");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+
+const MongoDbStore = require("connect-mongodb-session")(session);
+
+app.use(express.json());
+app.use(cookieParser());
+const store = new MongoDbStore({
+  uri: process.env.MONGODB_URL,
+  collection: "sessions",
+});
 
 app.use(
-  cors({
-    origin: [process.env.URL_CUSTOM, process.env.URL_CUSTOM2],
+  cros({
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: "lax",
+      secure: false,
+      maxAge: 86400000,
+    },
+    store: store,
+  })
+);
 
-app.use("/api", (req, res, next) => {
+//router
+app.use("/", (req, res, next) => {
   res.send("hello word");
 });
 
-app.listen(process.env.port, () => {
-  console.log("SERVER IS RUNNING");
-});
+const port = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const server = app.listen(port, () =>
+      console.log(`Server is running on port ${port}`)
+    );
+    const io = require("./utils/socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("connected socket", socket.id);
+
+      socket.on("send_message", (data) => {
+        console.log(data);
+        console.log(data.name + ":" + data.message);
+        io.emit("receive_message", data);
+      });
+      socket.on("disconnect", () => {
+        console.log("disconnected socket", socket.id);
+      });
+    });
+  })
+  .catch((err) => console.log(err));
